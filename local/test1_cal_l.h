b9eb70ee286361f5920ee41b38707a7fa58e4f13
jamesj <ehddnr177@uangel.com>
2020-08-14 04:37:41 -0400

    Modify codes

--- local/test1_cal_l.h
+++ local/test1_cal_l.h
@@ -7,32 +7,45 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdint.h>
+#include <limits.h>
 
+/** 성공 여부 */
 #define CAL_SUCCESS 1
+/** 실패 여부 */
 #define CAL_FAIL 0
+/** default value to calculate */
+#define DEFAULT_INT INT_MAX
+/** max number of calculation */
+#define MAX_NCAL 4
 
 typedef struct test1_cal_s test1_cal_t;
+typedef int (*calculate_f)( test1_cal_t *cal);
+
+/**@brief test1_cal_t struct */
 struct test1_cal_s{
-	uint16_t is_div_success;
-	int val1;
-	int val2;
-	int result_plus;
-	int result_minus;
-	int result_mul;
-	double result_div;
+	uint16_t is_div_success;	/**< 나눗셈 계산 성공 여부 */
+	int val1;					/**< first value to calculate */
+	int val2;					/**< second value to calculate */
+	int result_add;				/**< 덧셈 결과 */
+	int result_sub;				/**< 뺄셈 결과 */
+	int result_mul;				/**< 곱셈 결과 */
+	double result_div;			/**< 나눗셈 결과 */
+	int seq_list[ MAX_NCAL];	/**< 출력 순서 목록 */
+	calculate_f func[MAX_NCAL];	/**< 사칙연산 지정 함수 포인터 */
 };
 
 //////////////////////////////////////
 // function for test1_cal_t
 //////////////////////////////////////
-test1_cal_t* test1_cal_create( int val1, int val2);
-int test1_cal_init( test1_cal_t *cal, int val1, int val2);
+test1_cal_t* test1_cal_create();
+int test1_cal_init( test1_cal_t *cal);
 void test1_cal_final( test1_cal_t *cal);
-void test1_cal_destroy( test1_cal_t *cal);
+void test1_cal_destroy( test1_cal_t **cal);
 
-int test1_cal_plus( test1_cal_t *cal);
-int test1_cal_minus( test1_cal_t *cal);
+int test1_cal_add( test1_cal_t *cal);
+int test1_cal_sub( test1_cal_t *cal);
 int test1_cal_mul( test1_cal_t *cal);
 int test1_cal_div( test1_cal_t *cal);
 void test1_cal_print_all( test1_cal_t *cal);
--- test1_cal.c
+++ test1_cal.c
@@ -1,7 +1,22 @@
 #include "local/test1_cal_l.h"
 
-test1_cal_t* test1_cal_create( int val1, int val2){
-	int rv;
+//////////////////////////////////////
+// static function for test1_cal_t
+//////////////////////////////////////
+static void test1_cal_get_values( test1_cal_t *cal);
+static void test1_cal_get_seq( test1_cal_t *cal);
+static int test1_cal_input_data( int *val);
+
+//////////////////////////////////////
+// local function for test1_cal_t
+//////////////////////////////////////
+/**
+ * @fn test1_cal_t* test1_cal_create( int val1, int val2)
+ * @brief function to create test1_cal_t struct object
+ * @return test1_cal_t struct object
+ */
+test1_cal_t* test1_cal_create(){
+	int rv = CAL_FAIL;
 
 	test1_cal_t *cal = ( test1_cal_t*)malloc( sizeof( test1_cal_t));
 	if( cal == NULL){
@@ -9,7 +24,8 @@ test1_cal_t* test1_cal_create( int val1, int val2){
 		return NULL;
 	}
 
-	rv = test1_cal_init( cal, val1, val2); if( rv < CAL_SUCCESS){
+	rv = test1_cal_init( cal);
+	if( rv < CAL_SUCCESS){
 		printf("	| ! Fail to initialize test1_cal_t.\n");
 		return NULL;
 	}
@@ -17,52 +33,107 @@ test1_cal_t* test1_cal_create( int val1, int val2){
 	return cal;
 }
 
-int test1_cal_init( test1_cal_t *cal, int val1, int val2){
-	cal->is_div_success = 0;
-	cal->val1 = val1;
-	cal->val2 = val2;
-	cal->result_plus = 0;
-	cal->result_minus = 0;
-	cal->result_mul = 0;
-	cal->result_div = 0;
-
+/**
+ * @fn int test1_cal_init( test1_cal_t *cal)
+ * @brief function to initialize test1_cal_t struct object
+ * @param cal test1_cal_t struct object to initialize
+ * @return success
+ */
+int test1_cal_init( test1_cal_t *cal){
+	if( cal != NULL){
+		memset( cal, 0, sizeof( test1_cal_t));
+		test1_cal_get_values( cal);
+		test1_cal_get_seq( cal);
+		cal->func[0] = test1_cal_add;
+		cal->func[1] = test1_cal_sub;
+		cal->func[2] = test1_cal_mul;
+		cal->func[3] = test1_cal_div;
+	}
+	else return CAL_FAIL;
 	return CAL_SUCCESS;
 }
 
+/**
+ * @fn void test1_cal_final( test1_cal_t *cal)
+ * @brief function to finalize test1_cal_t struct object, destroy memory of member objects
+ * @param cal test1_cal_t struct object to finalize
+ * @return void
+ */
 void test1_cal_final( test1_cal_t *cal){
 }
 
-void test1_cal_destroy( test1_cal_t *cal){
-	test1_cal_final( cal);
-	free( cal);
+/**
+ * @fn void test1_cal_destroy( test1_cal_t **cal)
+ * @brief function to detroy memory of test1_cal_t struct object
+ * @param cal test1_cal_t struct object to destroy
+ * @return void
+ */
+void test1_cal_destroy( test1_cal_t **cal){
+	test1_cal_final( *cal);
+	free( *cal);
+	*cal = NULL;
 }
 
+/**
+ * @fn void test_cal_clear( test1_cal_t *cal)
+ * @brief function to clear data of test1_cal_t struct object
+ * @param cal test1_cal_t struct object to clear
+ * @return void
+ */
 void test_cal_clear( test1_cal_t *cal){
-	cal->is_div_success = 0;
-	cal->val1 = 0;
-	cal->val2 = 0;
-	cal->result_plus = 0;
-	cal->result_minus = 0;
-	cal->result_mul = 0;
-	cal->result_div = 0;
+	if( cal != NULL) memset( cal, 0, sizeof( test1_cal_t));
 }
 
-int test1_cal_plus( test1_cal_t *cal){
-	cal->result_plus = cal->val1 + cal->val2;
+/**
+ * @fn int test1_cal_add( test1_cal_t *cal)
+ * @brief function to add data of test1_cal_t struct object
+ * @param cal test1_cal_t struct object to add
+ * @return success
+ */
+int test1_cal_add( test1_cal_t *cal){
+	if( cal == NULL) return CAL_FAIL;
+
+	cal->result_add = cal->val1 + cal->val2;
+	printf("	| @ Plus	: (%d) + (%d) = %d\n", cal->val1, cal->val2, cal->result_add);
 	return CAL_SUCCESS;
 }
 
-int test1_cal_minus( test1_cal_t *cal){
-	cal->result_minus = cal->val1 - cal->val2;
+/**
+ * @fn int test1_cal_sub( test1_cal_t *cal)
+ * @brief function to sub data of test1_cal_t struct object
+ * @param cal test1_cal_t struct object to sub
+ * @return success
+ */
+int test1_cal_sub( test1_cal_t *cal){
+	if( cal == NULL) return CAL_FAIL;
+
+	cal->result_sub = cal->val1 - cal->val2;
+	printf("	| @ Minus	: (%d) - (%d) = %d\n", cal->val1, cal->val2, cal->result_sub);
 	return CAL_SUCCESS;
 }
 
+/**
+ * @fn int test1_cal_mul( test1_cal_t *cal)
+ * @brief function to multiply data of test1_cal_t struct object
+ * @param cal test1_cal_t struct object to multiply
+ * @return success
+ */
 int test1_cal_mul( test1_cal_t *cal){
+	if( cal == NULL) return CAL_FAIL;
+
 	cal->result_mul = cal->val1 * cal->val2;
+	printf("	| @ Multiply	: (%d) x (%d) = %d\n", cal->val1, cal->val2, cal->result_mul);
 	return CAL_SUCCESS;
 }
 
+/**
+ * @fn int test1_cal_div( test1_cal_t *cal)
+ * @brief function to divide data of test1_cal_t struct object
+ * @param cal test1_cal_t struct object to divide
+ * @return success
+ */
 int test1_cal_div( test1_cal_t *cal){
+	if( cal == NULL) return CAL_FAIL;
 	if( cal->val2 == 0){
 		return CAL_FAIL;
 	}
@@ -70,15 +141,98 @@ int test1_cal_div( test1_cal_t *cal){
 		cal->result_div = (double)cal->val1 / (double)cal->val2;
 		cal->is_div_success++;
 	}
+
+	if( cal->is_div_success == CAL_SUCCESS) printf("	| @ Divide	: (%d) / (%d) = %0.2f\n", cal->val1, cal->val2, cal->result_div);
+	else if( cal->is_div_success == CAL_FAIL) printf("	| ! Fail to calculate divide. val2 is '0' (val1:%d / val2:%d)\n", cal->val1, cal->val2);
+
 	return CAL_SUCCESS;
 }
 
+/**
+ * @fn void test1_cal_print_all( test1_cal_t *cal)
+ * @brief function to print data of test1_cal_t struct object
+ * @param cal test1_cal_t struct object to print
+ * @return void
+ */
 void test1_cal_print_all( test1_cal_t *cal){
-	printf("	| @ Input1	: %d / Input2 : %d\n", cal->val1, cal->val2);
-	printf("	| @ Plus	: (%d) + (%d) = %d\n", cal->val1, cal->val2, cal->result_plus);
-	printf("	| @ Minus	: (%d) - (%d) = %d\n", cal->val1, cal->val2, cal->result_minus);
-	printf("	| @ Multiply	: (%d) x (%d) = %d\n", cal->val1, cal->val2, cal->result_mul);
-	if( cal->is_div_success == CAL_SUCCESS) printf("	| @ Divide	: (%d) / (%d) = %0.2f\n", cal->val1, cal->val2, cal->result_div);
-	else if( cal->is_div_success == CAL_FAIL) printf("	| ! Fail to calculate divide. val2 is '0' (val1:%d / val2:%d)\n", cal->val1, cal->val2);
+	if( cal == NULL) return ;
+
+	printf("\n\t| @ Input1	: %d / Input2 : %d\n", cal->val1, cal->val2);
+
+	int rv = 0, i;
+	for( i = 0; i < MAX_NCAL; i++){
+		rv = cal->func[ cal->seq_list[i] - 1]( cal);
+		if( rv < CAL_SUCCESS) printf("\t| ! Fail to calculate values for test1_cal_t. (index:%d)\n", i);
+	} 
+}
+
+static void test1_cal_get_values( test1_cal_t *cal){
+	/** return value to check error for val1 */
+	/** return value to check error for val2 */
+	int rv_val1 = CAL_FAIL, rv_val2 = CAL_FAIL;
+	cal->val1 = DEFAULT_INT;
+	cal->val2 = DEFAULT_INT;
+
+	while( 1){
+		if(( cal->val1 == DEFAULT_INT) && ( rv_val1 == CAL_FAIL)){
+			printf("\t| @ Enter first value\t: ");
+			rv_val1 = test1_cal_input_data( &cal->val1);
+			if( rv_val1 < CAL_SUCCESS) continue;
+		}
+
+		if(( cal->val2 == DEFAULT_INT) && ( rv_val2 == CAL_FAIL)){
+			printf("\t| @ Enter second value\t: ");
+			rv_val2 = test1_cal_input_data( &cal->val2);
+			if( rv_val2 < CAL_SUCCESS) continue;
+		}
+
+		if(( cal->val1 != DEFAULT_INT) && ( cal->val2 != DEFAULT_INT) && ( rv_val1 == CAL_SUCCESS) && ( rv_val2 == CAL_SUCCESS)) break;
+	}
+}
+
+static void test1_cal_get_seq( test1_cal_t *cal){
+	int i;
+	/** sequence number to print data for test1_cal_t */
+	int seq = DEFAULT_INT;
+	/** return value to check error for seq */
+	int rv_seq = CAL_FAIL;
+	/** count value for seq */
+	int seq_count = CAL_FAIL;
+
+	printf("\n\t| @ Sequence number decision\n");
+	printf("\t| @ [1:add] [2:sub] [3:mul] [4:div]\n");
+	while( 1){
+		if(( seq == DEFAULT_INT) && ( rv_seq == CAL_FAIL)){
+			printf("\t| @ Enter sequence number (%d)\t: ", (seq_count+1));
+			rv_seq = test1_cal_input_data( &seq);
+			if( rv_seq < CAL_SUCCESS) continue;
+			if(( seq <= 0) && ( seq > 4)){
+				printf("\t| ! Wrong sequence number, value is invalid!\n");
+				seq = DEFAULT_INT;
+				continue;
+			}
+
+			cal->seq_list[seq_count] = seq;
+			seq_count++;
+
+			if( seq_count < MAX_NCAL){
+				seq = DEFAULT_INT;
+				rv_seq = CAL_FAIL;
+			}
+		}
+
+		if(( seq != DEFAULT_INT) && ( rv_seq == CAL_SUCCESS) && ( seq_count == MAX_NCAL)) break;
+	}
+}
+
+static int test1_cal_input_data( int *val){
+	int rv = scanf( "%d", val);
+	if( rv == CAL_FAIL){
+		printf("\t| ! Wrong value, value is not integer!\n");
+		*val = DEFAULT_INT;
+		while( getchar() != '\n');
+		return rv;
+	}
+	return rv;
 }
 
