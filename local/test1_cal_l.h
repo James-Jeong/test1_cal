226d5ee2978396a4167db96e659c287ac9280943
jamesj <ehddnr177@uangel.com>
2020-08-18 00:51:37 -0400

    Modfiy codes

--- local/test1_cal_l.h
+++ local/test1_cal_l.h
@@ -21,19 +21,19 @@
 #define MAX_NCAL 4
 
 typedef struct test1_cal_s test1_cal_t;
-typedef int (*calculate_f)( test1_cal_t *cal);
+typedef int (*test1_cal_operation_f)( test1_cal_t *cal);
 
 /**@brief test1_cal_t struct */
 struct test1_cal_s{
-	uint16_t is_div_success;	/**< 나눗셈 계산 성공 여부 */
-	int val1;					/**< first value to calculate */
-	int val2;					/**< second value to calculate */
-	int result_add;				/**< 덧셈 결과 */
-	int result_sub;				/**< 뺄셈 결과 */
-	int result_mul;				/**< 곱셈 결과 */
-	double result_div;			/**< 나눗셈 결과 */
-	int seq_list[ MAX_NCAL];	/**< 출력 순서 목록 */
-	calculate_f func[MAX_NCAL];	/**< 사칙연산 지정 함수 포인터 */
+	uint16_t is_div_success;				/**< 나눗셈 계산 성공 여부 */
+	int val1;								/**< first value to calculate */
+	int val2;								/**< second value to calculate */
+	int result_add;							/**< 덧셈 결과 */
+	int result_sub;							/**< 뺄셈 결과 */
+	int result_mul;							/**< 곱셈 결과 */
+	double result_div;						/**< 나눗셈 결과 */
+	int seq_list[ MAX_NCAL];				/**< 출력 순서 목록 */
+	test1_cal_operation_f func[MAX_NCAL];	/**< 사칙연산 지정 함수 포인터 */
 };
 
 //////////////////////////////////////
@@ -47,5 +47,5 @@ void test1_cal_destroy( test1_cal_t **cal);
 void test1_cal_print_all( test1_cal_t *cal);
 void test_cal_clear( test1_cal_t *cal);
 
-#endif // __TEST1_CAL_H__
+#endif // #ifndef __TEST1_CAL_H__
 
